305280c9844f1f00a607fbb220d842ae1ad66f21
Ben McAllister <McAJBen@gmail.com>
2023-07-14 23:53:59 -0400

    Use dynamic height for mobile views

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
     <iframe
-      className="block h-[calc(100vh-5rem)] w-full"
+      className="block h-[calc(100dvh-5rem)] w-full"
       src="/BenMcAllisterResume.pdf"
       title="Resume"
     />
