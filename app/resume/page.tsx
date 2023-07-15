3af468c236ba79f1467c383ed704c61000892a58
Ben McAllister <McAJBen@gmail.com>
2023-07-15 00:39:32 -0400

    Use google pdf viewer

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <iframe
       className="block h-[calc(100dvh-5rem)] w-full"
-      src="/BenMcAllisterResume.pdf"
+      src="https://docs.google.com/viewer?embedded=true&url=https%3A%2F%2Fwww.mcajben.com%2FBenMcAllisterResume.pdf"
       title="Resume"
     />
   );
