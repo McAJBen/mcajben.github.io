80c26e4d4482a81b08c7f8f8c87f5e11247a9594
Ben McAllister <McAJBen@gmail.com>
2023-07-15 02:46:17 -0400

    Fix other text links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,24 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 flex h-8 w-full items-center justify-center bg-[--color-background-light] text-xs text-white">
-      <Link className="text-secondary" href="https://LinkedIn.com/in/McAJBen">
+      <Link
+        className="text-secondary hover:text-white"
+        href="https://LinkedIn.com/in/McAJBen"
+      >
         LinkedIn
       </Link>
       &nbsp;|&nbsp;
-      <Link className="text-secondary" href="http://GitHub.com/McAJBen">
+      <Link
+        className="text-secondary hover:text-white"
+        href="http://GitHub.com/McAJBen"
+      >
         GitHub
       </Link>
       &nbsp;|&nbsp;
-      <Link className="text-secondary" href="mailto:McAJBen@gmail.com">
+      <Link
+        className="text-secondary hover:text-white"
+        href="mailto:McAJBen@gmail.com"
+      >
         Contact
       </Link>
     </footer>
